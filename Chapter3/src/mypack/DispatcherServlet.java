f4ae5a9b41123b6e2ddfff3a96cf43d6c1e3a062
Faded828xx <zhengyouheng732@gmail.com>
2021-07-09 17:16:55 +0800

    Tomcat部署JavaWeb

---
+++ Chapter3/src/mypack/DispatcherServlet.java
@@ -0,0 +1,16 @@
+package mypack;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class DispatcherServlet extends GenericServlet {
+    private String target = "/index.jsp";
+
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        request.setAttribute("USER", username);
+        ServletContext context = request.getServletContext();
+        RequestDispatcher dispatcher = context.getRequestDispatcher(target);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ Chapter3/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: faded828x
+  Date: 2021/7/9
+  Time: 16:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  Hello: <%= request.getAttribute("USER") %>
+  </body>
+</html>
